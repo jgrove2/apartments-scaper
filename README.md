4532cb053decc1a6eb984081ab3df6f0a209c6dd
Justin Grove <26723005+jgrove2@users.noreply.github.com>
2024-05-18 13:52:24 -0400

    Update Readme (#23)

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# apartments-scaper
+# Apartments.com Scraper
+The apartments.com scraper is given a url for a outlined area from apartments.com and over the course of time that it is running it will scrape and update the user on new apartments that appear fitting the description they want. If a new apartment appears it will send a message to a discord channel. The user can also ask to view all apartments currently listed that fit their description.
+
+## Setup
+
+### Prerecs
+
+| Technology | Version |
+| ---------- | -------- |
+| Bun | 1.0.0 |
+
+### Install
+
+```bash
+git clone {repo}
+cd apartment-scraper
+```
+
+### Build/Run
+
+```bash
+npm run start
+```
+
+## Instructions
+
+### Discord Bot
+
+### Scraper
--- src/discordbot/base/classes/Database.ts
+++ src/discordbot/base/classes/Database.ts
@@ -8,9 +8,13 @@ import { Client } from "discord.js";
 export default class Database implements IDatabase {
   db: sqlite;
   logger: Logger;
+  minBathroom: string;
+  minBedrooms: string;
   constructor() {
     this.db = new sqlite("./db/apartments.db");
     this.logger = new Logger();
+    this.minBathroom = Bun.env.MIN_BATHROOMS ?? "1";
+    this.minBedrooms = Bun.env.MIN_BEDROOMS ?? "2";
   }
   async selectNewApartments(): Promise<any[]> {
     this.logger.logInfo("selectNewApartments - Starting function");
@@ -22,7 +26,7 @@ export default class Database implements IDatabase {
   getAllData(): string {
     this.logger.logInfo("getAllData - Starting function");
     let query = this.db.query(
-      `SELECT * FROM apartments WHERE bathrooms >= 2 AND bedrooms = 4`
+      `SELECT * FROM apartments WHERE bathrooms >= ${this.minBathroom} AND bedrooms = ${this.minBedrooms}`
     );
     let result = query.all();
     let resultString = "";
